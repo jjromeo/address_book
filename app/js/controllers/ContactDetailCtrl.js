867dff6cb60d4e0cb5d369e47052ef56d4e95523
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-17 23:46:43 +0000

    can now delete contacts on their contact page

--- app/js/controllers/ContactDetailCtrl.js
+++ app/js/controllers/ContactDetailCtrl.js
@@ -1,10 +1,16 @@
 'use strict';
 
-addressBookControllers.controller('ContactDetailCtrl', ['$scope', '$http','$routeParams',
-  function($scope, $http, $routeParams){
+addressBookControllers.controller('ContactDetailCtrl', ['$scope', '$http','$routeParams','$location',
+  function($scope, $http, $routeParams, $location){
     $http.get('http://fast-gorge.herokuapp.com/contacts/' + $routeParams.id).success(function(data){
       $scope.contact = data
     })
+
+   $scope.deleteContact = function(contactID) {
+     $http.delete('http://fast-gorge.herokuapp.com/contacts/' + contactID).success(function(data){
+       $location.path("/contacts");
+     })
+   }
   }
 ]);
                                   
--- app/js/controllers/ContactListCtrl.js
+++ app/js/controllers/ContactListCtrl.js
@@ -5,6 +5,7 @@ addressBookControllers.controller('ContactListCtrl', ['$scope', '$http','$filter
    $http.get('http://fast-gorge.herokuapp.com/contacts').success(function(data) {
      $scope.contacts = $filter('orderBy')(data, 'first_name')
    });
+
  }
 
 ]);
