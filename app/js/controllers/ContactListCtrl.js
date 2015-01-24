4a372ca8d98f4312a54087a5a071d7f8eae4c3ce
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-24 22:57:29 +0000

    finally figured out how to sort contacts from resource - had to pass in callback when querying

--- app/js/controllers/ContactListCtrl.js
+++ app/js/controllers/ContactListCtrl.js
@@ -2,8 +2,9 @@
 
 addressBookControllers.controller('ContactListCtrl', ['$scope', '$http','$filter', 'Alerter', '$log', 'ContactService',
  function ($scope, $http, $filter, Alerter, $log, ContactService) {
-     $scope.contacts = ContactService.contacts.query();
-     $scope.sortedContacts = $filter('orderBy')($scope.contacts, 'first_name')
+     ContactService.contacts.query(function(contacts){
+       $scope.contacts = $filter('orderBy')(contacts, 'first_name')
+     });
  }
 
 ]);
